678cef2b4776e50b487dfad444bcfa911d412509
Nathan Alex <nate745@gmail.com>
2019-07-10 05:29:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ function multiply (x, y){
 function divide (x, y){
   return x / y
 }
+
+
+var number = 10
+
+function add5() {
+  number += 5
+}
+
+function divideBy3() {
+  number /= 3
+}
+
+divideBy3()
+console.log(number)
+
+add5()
+console.log(number)
+
+
+number = 10
+
+add5()
+
+console.log(number) // 15
+
+divideBy3()
+
+console.log(number) // 5
+
