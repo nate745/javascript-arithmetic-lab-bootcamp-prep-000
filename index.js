a05f70f15ea5a39373434b9a70a4c70bef717d91
Nathan Alex <nate745@gmail.com>
2019-07-10 05:16:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function add (x + y){
+function add (x, y){
   return x + y
 }
-function subtract (x - y){
+function subtract (x, y){
   return x - y
 }
-function multiply (x * y){
+function multiply (x, y){
   return x * y
 }
-function divide (x / y){
+function divide (x, y){
   return x / y
 }
